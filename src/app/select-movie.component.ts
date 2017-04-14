be28956855e8133d5cf92bf209f3fe665ed56036
vivian <zlh.workspace@gmail.com>
2017-04-14 19:42:48 +0800

    modify the method of selecting

--- src/app/select-movie.component.ts
+++ src/app/select-movie.component.ts
@@ -10,6 +10,7 @@ import { OnInit } from '@angular/core';
 })
 export class SelectMovieComponent implements OnInit {
     movies: MOVIE[];
+    selectedStyle: MOVIE[];
     selectedMovies: MOVIE[];
     constructor(private movieService: MovieService){}
     getMovies(): void {
@@ -19,12 +20,35 @@ export class SelectMovieComponent implements OnInit {
         this.getMovies();
     }
     selectMovies(moviestyle:string,movietime:string): void{
-        for (var i=0;this.movies[i];i++){
-            if (this.movies[i].MovieStyle=moviestyle){
-                if((movietime='2015-2018')&&(this.movies[i].TimeNum<=2018)&&this.movies[i].TimeNum>=2015){
-                    this.selectedMovies.push(this.movies[i]);
+        if(moviestyle=='any'){
+            this.selectedStyle=this.movies;
+        }
+        else{
+            for(let movie of this.movies){
+                if(movie.MovieStyle==moviestyle){
+                    this.selectedStyle.push(movie);              
                 }
             }
         }
+        if(movietime=='any'){
+            this.selectedMovies=this.selectedStyle;
+        }
+        else{
+            for(let movie of this.selectedStyle){
+                if((movietime=='1950-2000')&&(movie.ShowTime<'2000-01-01')&&(movie.ShowTime>='1950=01=01')){
+                    this.selectedMovies.push(movie);
+                }
+                else if((movietime=='2000-2010')&&(movie.ShowTime<'2010-01-01')&&(movie.ShowTime>='2000-01-01')){
+                    this.selectedMovies.push(movie);
+                }
+                else if((movietime=='2010-2015')&&(movie.ShowTime<'2015-01-01')&&(movie.ShowTime>='2010-01-01')){
+                    this.selectedMovies.push(movie);
+                }
+                else if((movietime=='2015-2018')&&(movie.ShowTime<'2018-01-01')&&(movie.ShowTime>='2015-01-01')){
+                    this.selectedMovies.push(movie);
+                }
+            }
+        }
+        
     }
 }
