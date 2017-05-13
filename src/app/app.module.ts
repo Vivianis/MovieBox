3711cebb050d2f26e44532bd30362c7b165cd783
fengzhigang <fengzg@polyvi.com>
2017-05-13 13:44:27 +0800

    1）移除了模拟数据，改为访问真实的后台 2）其他微调 3）fix了css中的一个bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,42 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 import { LogInComponent } from './log-page/log-in.component';
 import { SelectMovieComponent } from './display/select-movie.component';
 import { MovieRecommendComponent } from './first-page/movie-recommend.component';
 import { MovieDetailComponent } from './detail/movie-detail.component';
 import { BuyMovieComponent } from './buy/buy-movie.component';
 import { NoPageComponent } from './wrong-page/no-page.component';
-import { MovieService } from './movie.service'; 
-
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
+import { MovieService } from './movie.service';
 
 import { ValidSeat } from './seat-style/valid-seat.component';
 import { SelectedSeat } from './seat-style/selected-seat.component';
-import { InvalidSeat } from './seat-style/invalid-seat.component'; 
+import { InvalidSeat } from './seat-style/invalid-seat.component';
 
 @NgModule({
-  imports:      [ BrowserModule,
-                  FormsModule,
-                  HttpModule,
-                  JsonpModule,
-                  InMemoryWebApiModule.forRoot(InMemoryDataService),
-                  AppRoutingModule
-                ],
-  declarations: [ AppComponent,
-                  LogInComponent,
-                  SelectMovieComponent,
-                  MovieRecommendComponent,
-                  MovieDetailComponent,
-                  BuyMovieComponent,
-                  NoPageComponent,
-                  ValidSeat,
-                  SelectedSeat,
-                  InvalidSeat
-                ],
-  bootstrap:    [ AppComponent ],
-  providers:    [ MovieService ]
+  imports: [BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
+    AppRoutingModule
+  ],
+  declarations: [AppComponent,
+    LogInComponent,
+    SelectMovieComponent,
+    MovieRecommendComponent,
+    MovieDetailComponent,
+    BuyMovieComponent,
+    NoPageComponent,
+    ValidSeat,
+    SelectedSeat,
+    InvalidSeat
+  ],
+  bootstrap: [AppComponent],
+  providers: [MovieService]
 })
-export class AppModule {}
+export class AppModule { }
